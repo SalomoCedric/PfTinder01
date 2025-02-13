305174c1d7cf515ee9fe0dbb1eea233b1ed735f2
SalomoCedric <salomokarst19@gmail.com>
2025-02-13 20:14:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBEjyN4VabIOl6eOy3o4iQndzDiKve-B8A",
   authDomain: "pfortetinder.firebaseapp.com",
@@ -7,6 +15,10 @@ const firebaseConfig = {
   appId: "1:595874292907:web:d0e6e24047960c7726c3ee",
   measurementId: "G-YTDLD3T4P8"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
